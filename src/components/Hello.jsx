d7d9303c03bba4a5105a28fe7ee32e214ec7b2d9
ColinAdo <colinadodev30@gmail.com>
2024-04-22 07:41:30 +0300

    Add working with JSX

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,6 +1,24 @@
+// JSX => stands for Javascript synatx, is way of combining javascript and html syntax
+// JSX Role
+// JSX can only return one content except they are wrapped in an enclosing tag i.e <div></div> 
+
+// const name = "Jones Doe";
+
+function greeting() {
+    return 'How is react js??'
+}
+
+// function Hello() {
+//     return <h2>Hello World, from components,  {greeting()}</h2>
+// }
 
 function Hello() {
-    return <h2>Hello World, from components...</h2>
+    return (
+        <div>
+            <h2>Hello World, from components,  {greeting()}</h2>
+            <h3>This is nice place to be!</h3>
+        </div>
+    );
 }
 
 export default Hello;
