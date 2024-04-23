8088806936d51296311b3a98e873c68b5a501570
ColinAdo <colinadodev30@gmail.com>
2024-04-23 08:34:16 +0300

    Add object propbs to a component

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,19 @@ import Hello from './components/Hello';
 
 function App() {
   {/* Passing array t component */}
-  const nickNames = ['Deluxe', 'Sam'];
+  // const nickNames = ['Deluxe', 'Sam'];
+
+  // Pasing object props to components
+  const person = {
+    name: 'John',
+    age: 36,
+    phoneNumbers: ['070712222', '07828921892']
+  }
   
   return (
     <div className="App">
-      <Hello nickNames={nickNames} />
+      {/* <Hello nickNames={nickNames} /> */}
+      <Hello person={person} />
     </div>
   )
 }
--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,8 +1,11 @@
 // Working with passing array and objects props to a component
-function Hello(props) {
+function Hello({person}) {
     return (
         <div>
-            <h3>My nick names are: {props.nickNames}</h3>
+            {/* <h3>My nick names are: {nickNames}</h3> */}
+            <h4>{person.name}</h4>
+            <h4>{person.age}</h4>
+            <h4>{person.phoneNumbers}</h4>
         </div>
     );
 }
