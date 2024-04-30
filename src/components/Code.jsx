34bd789eff7e419c1a4348082666a170a3856590
ColinAdo <colinadodev30@gmail.com>
2024-04-30 21:08:41 +0300

    Add event handler in react

--- src/components/Code.jsx
+++
@@ -1,7 +0,0 @@
-export default function Code() {
-    return (
-        <div>
-            <h2>Coding is fun!</h2>
-        </div>
-    )
-}
--- src/components/Condition.jsx
+++
@@ -1,7 +0,0 @@
-import Code from "./Code";
-import Greetings from "./Greetings";
-
-export default function Condition() {
-    const display = true;
-    return display ? <Code /> : <Greetings />;
-}
